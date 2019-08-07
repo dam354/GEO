2bcdadff6baa62e0090a082f1b4f6702983bf299
Dan Mennella <Dmennella34@gmail.com>
2019-08-07 16:23:02 +0000

    first commit

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby"  arbitrartPhrase="is arbitrary"/>
+    <p>Such wow. Very React.</p>
+  </div>
+)
